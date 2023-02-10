e8197cc5ec203b7f025938a29ab7464622e74459
Josh Landsman <jlandsman@gatech.edu>
2023-02-10 12:39:26 -0500

    route summaries

--- Backend/src/api/teams/teams.controller.ts
+++ Backend/src/api/teams/teams.controller.ts
@@ -1,4 +1,9 @@
-import { ApiBearerAuth, ApiQuery, ApiTags } from "@nestjs/swagger";
+import {
+	ApiBearerAuth,
+	ApiOperation,
+	ApiQuery,
+	ApiTags
+} from "@nestjs/swagger";
 import { Body, Controller, Delete, Get, Post, Query } from "@nestjs/common";
 import { TeamsService } from "../../domain/teams/teams.service";
 import { Team } from "../../data/entities/Team";
@@ -14,49 +19,58 @@ export class TeamsController {
 
 	constructor(private readonly teamsService: TeamsService) {}
 
+	@ApiOperation({ summary: "Create a new team" })
 	@Post("create")
 	async create(@Body() teamDTO: TeamCreateDto): Promise<Team> {
 		return this.teamsService.create(teamDTO);
 	}
 
+	@ApiOperation({ summary: "Get all teams" })
 	@Get("all")
 	async getAll(): Promise<Team[]> {
 		return this.teamsService.getAll();
 	}
 
+	@ApiOperation({ summary: "Get a team by id" })
 	@ApiQuery({ name: "id", required: true })
 	@Get(":id")
 	async get(@Query("id") id: string): Promise<Team> {
 		return this.teamsService.get(id);
 	}
 
+	@ApiOperation({ summary: "Get a team by user id" })
 	@ApiQuery({ name: "id", required: true })
 	@Get("/members/:id")
 	async getWithUserId(@Query("id") id: string): Promise<Team> {
 		return this.teamsService.getWithUserId(id);
 	}
 
+	@ApiOperation({ summary: "Add a member to a team" })
 	@Post("members/add")
 	async addMember(@Body() teamModifyDto: TeamModifyDto): Promise<Team> {
 		return this.teamsService.addMember(teamModifyDto);
 	}
 
+	@ApiOperation({ summary: "Remove a member from a team" })
 	@Post("members/remove")
 	async removeMember(@Body() teamModifyDto: TeamModifyDto): Promise<Team> {
 		return this.teamsService.removeMember(teamModifyDto);
 	}
 
+	@ApiOperation({ summary: "Delete a team" })
 	@ApiQuery({ name: "id", required: true })
 	@Delete()
 	async delete(@Query("id") id: string): Promise<boolean> {
 		return this.teamsService.delete(id);
 	}
 
+	@ApiOperation({ summary: "Invite a member to a team" })
 	@Post("invites")
 	async inviteMember(@Body() teamModifyDto: TeamModifyDto): Promise<Team> {
 		return this.teamsService.inviteMember(teamModifyDto);
 	}
 
+	@ApiOperation({ summary: "Accept an invitation to a team" })
 	@Post("invites/accept")
 	async acceptInvitation(
 		@Body() teamModifyDto: TeamModifyDto
@@ -64,6 +78,7 @@ export class TeamsController {
 		return this.teamsService.acceptInvite(teamModifyDto);
 	}
 
+	@ApiOperation({ summary: "Reject an invitation to a team" })
 	@Post("invites/reject")
 	async rejectInvitation(
 		@Body() teamModifyDto: TeamModifyDto
@@ -71,6 +86,7 @@ export class TeamsController {
 		return this.teamsService.rejectInvite(teamModifyDto);
 	}
 
+	@ApiOperation({ summary: "Get all teams a user is invited to" })
 	@ApiQuery({ name: "id", required: true })
 	@Get("invites/member/:id")
 	async getInvitesForUser(@Query("id") id: string): Promise<Team[]> {
