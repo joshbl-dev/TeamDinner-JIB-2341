8e596148d7d1c218a4a79ed659aa3f3ea99cb062
Josh Landsman <jlandsman@gatech.edu>
2023-02-10 13:14:20 -0500

    testing swagger on prod

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -4,6 +4,8 @@ import { json, urlencoded } from "express";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import * as fs from "fs";
+import { createWriteStream } from "fs";
+import { get } from "http";
 
 async function bootstrap() {
 	const version = process.env.npm_package_version;
@@ -52,6 +54,50 @@ async function bootstrap() {
 			process.env.PORT +
 			"/api"
 	);
+
+	if (process.env.NODE_ENV === "dev") {
+		const serverUrl =
+			"http://" + process.env.ADDRESS + ":" + process.env.PORT;
+		// write swagger ui files
+		get(`${serverUrl}/swagger/swagger-ui-bundle.js`, function (response) {
+			response.pipe(
+				createWriteStream("swagger-static/swagger-ui-bundle.js")
+			);
+			console.log(
+				`Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`
+			);
+		});
+
+		get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
+			response.pipe(
+				createWriteStream("swagger-static/swagger-ui-init.js")
+			);
+			console.log(
+				`Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`
+			);
+		});
+
+		get(
+			`${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
+			function (response) {
+				response.pipe(
+					createWriteStream(
+						"swagger-static/swagger-ui-standalone-preset.js"
+					)
+				);
+				console.log(
+					`Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`
+				);
+			}
+		);
+
+		get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
+			response.pipe(createWriteStream("swagger-static/swagger-ui.css"));
+			console.log(
+				`Swagger UI css file written to: '/swagger-static/swagger-ui.css'`
+			);
+		});
+	}
 }
 
 bootstrap();
