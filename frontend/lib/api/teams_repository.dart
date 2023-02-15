6033ed1888270f30cdec134d3963f2af7aa64a9d
shrutivedula <slvedula@gmail.com>
2023-02-15 16:21:18 -0500

    Created teams repo
    Created teams repo in the same folder as users repo

---
+++ frontend/lib/api/teams_repository.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../Types/token.dart';
+import '../Types/user.dart';
+
+class TeamsRepository {
+  static const String baseUrl = "https://team-dinner-jib-2341.vercel.app";
+  static final Map<String, String> headers = <String, String>{
+    'Content-Type': 'application/json; charset=UTF-8',
+  };
+  static const String repositoryName = "users";
+}
