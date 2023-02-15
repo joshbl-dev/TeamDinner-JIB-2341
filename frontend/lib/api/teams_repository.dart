97bbe7a5939673e586dbeade4cdbb403610b9557
shrutivedula <slvedula@gmail.com>
2023-02-15 16:37:21 -0500

    Added route to create in teams
    Added route in teams repo to create a team

--- frontend/lib/api/teams_repository.dart
+++ frontend/lib/api/teams_repository.dart
@@ -11,4 +11,24 @@ class TeamsRepository {
     'Content-Type': 'application/json; charset=UTF-8',
   };
   static const String repositoryName = "teams";
+
+  //create
+  static Future<User> create(String name, String description, String owner) async {
+    final response = await http.post(
+      Uri.parse("$baseUrl/$repositoryName/signup"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'teamName': name,
+        "description": description,
+        "owner": owner
+      }),
+    );
+    if (response.statusCode == 201) {
+      return User.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to create team.');
+    }
+  }
 }
