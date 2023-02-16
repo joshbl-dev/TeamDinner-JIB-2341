5a0870bbb6f3a0c28b2275843cf67912f604e3b7
cwinata042 <77115831+cwinata042@users.noreply.github.com>
2023-02-16 17:06:12 -0500

    add teams/invites accept route

--- frontend/lib/api/teams_repository.dart
+++ frontend/lib/api/teams_repository.dart
@@ -11,4 +11,23 @@ class TeamsRepository {
     'Content-Type': 'application/json; charset=UTF-8',
   };
   static const String repositoryName = "teams";
+
+  //invites accept
+  static Future<User> acceptInvites(String teamId, String userId) async {
+    final response = await http.post(
+      Uri.parse("$baseUrl/$repositoryName/invites/accept"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'teamId': teamId,
+        'userId': userId
+      }),
+    );
+    if (response.statusCode == 201) {
+      return User.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to accept invite.');
+    }
+  }
 }
