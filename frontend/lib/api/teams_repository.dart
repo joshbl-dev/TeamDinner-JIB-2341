96017224358c80e09b622d64917ba7345533a42c
zacharythomasabraham <zachary.thomas.abraham@gmail.com>
2023-02-16 22:33:21 -0500

    teams/delete
    Added teams/delete route

--- frontend/lib/api/teams_repository.dart
+++ frontend/lib/api/teams_repository.dart
@@ -12,5 +12,19 @@ class TeamsRepository {
   };
   static const String repositoryName = "teams";
 
-  
+  static Future<User> delete(Token accessToken, String id) async {
+    final response = await http.delete(
+      Uri.parse("$baseUrl/$repositoryName?id=$id"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        "Authorization": "Bearer ${accessToken.token}"
+      },
+
+    );
+    if (response.statusCode == 200) {
+      return User.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Team not found.');
+    }
+  }
 }
