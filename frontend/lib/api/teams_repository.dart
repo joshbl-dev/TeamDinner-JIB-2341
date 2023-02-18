0b8b04bb0fc4e7c8f786fcbd882c306841db98ff
Josh Landsman <jlandsman@gatech.edu>
2023-02-18 11:10:01 -0500

    merge fix

--- frontend/lib/api/teams_repository.dart
+++ frontend/lib/api/teams_repository.dart
@@ -85,4 +85,22 @@ class TeamsRepository {
       throw Exception('Failed to login.');
     }
   }
+
+  static Future<User> invites(String teamID, String userID) async {
+    final response = await http.post(
+      Uri.parse("$baseUrl/$repositoryName/invites"),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'teamID': teamID,
+        'userID': userID
+      }),
+    );
+    if (response.statusCode == 201) {
+      return User.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to invite user.');
+    }
+  }
 }
