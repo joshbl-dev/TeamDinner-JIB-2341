3035dac668c302cc3ee296db8546e8553a336cb4
Josh Landsman <jlandsman@gatech.edu>
2023-04-23 10:06:12 -0400

    fix

--- frontend/lib/widgets/poll_form.dart
+++ frontend/lib/widgets/poll_form.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_polls/flutter_polls.dart';
 import 'package:frontend/api/polls_repository.dart';
 
 import '../Types/Poll.dart';
+import '../Types/poll_option.dart';
 import '../Types/vote.dart';
 import '../api/users_repository.dart';
-
-
 
 class PollForm extends StatefulWidget {
   final Poll poll;
